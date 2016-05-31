241fc714bf038c23d643825a35a602e37e17672b
christian.lalanne@zweicom.com <christian.lalanne@zweicom.com>
2016-05-31 00:05:54 -0400

    ADD: first code and cmakeLists

---
+++ main.cpp
@@ -0,0 +1,17 @@
+
+#include "benchmark/benchmark_api.h"
+
+const int ITERATIONS = 1000000;
+
+    void fooImpl() {}
+
+void foo(benchmark::State& state) {
+    while (state.KeepRunning()) {
+        fooImpl();
+    }
+}
+BENCHMARK(foo);
+
+BENCHMARK_MAIN();
+
+
