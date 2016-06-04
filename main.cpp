8b5de90ac626a91674b16e17741a3237da96aabc
Christian Lalanne <lalanne@gmail.com>
2016-06-04 19:53:49 -0400

    ADD: liking with libjs

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 
 #include "benchmark/benchmark_api.h"
+#include "jsapi.h"
+#include "js/Initialization.h"
+
+using namespace JS;
 
 const int ITERATIONS = 1000000;
 
-void fooImpl() {}
+void fooImpl() {
+    JS_Init();
+}
 
 void foo(benchmark::State& state) {
     while (state.KeepRunning()) {
