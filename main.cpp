ba5d22e27ff4d4823eb045a2849e2cd495035854
Christian Lalanne <lalanne@gmail.com>
2016-06-04 23:21:55 -0400

    ADD: more of the skeleton for spider monkey

--- main.cpp
+++ main.cpp
@@ -9,11 +9,19 @@ const int ITERATIONS = 1000000;
 
 int fooImpl() {
     // Initialize the JS engine.
-    if (!JS_Init()) return 1;
+    if(!JS_Init()) return 1;
 
     // Create a JS runtime.
     JSRuntime *rt = JS_NewRuntime(8L * 1024L * 1024L);
-    if (!rt) return 1;
+    if(!rt) return 1;
+
+    // Create a context.
+  //  JSContext *cx = JS_NewContext(rt, 8192);
+ //   if(!cx) return 1;
+
+//    JS_SetErrorReporter(rt, reportError);
+
+ //   int status = run(cx);
 }
 
 void foo(benchmark::State& state) {
